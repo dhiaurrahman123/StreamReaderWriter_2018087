1a12e583ff74cc15f682f475cebbb3dbf5048ba2
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-06-21 15:04:12 +0700

    menampilkan perubahan pada file

--- IoFile.cpp
+++ IoFile.cpp
@@ -32,7 +32,11 @@ int main()
 	cout << endl << ">= Membuka dan membaca file " << endl;
 	//jika file ada maka
 	if (infile.is_open()) {
-
+		//melakukan perulangan tiap baris
+		while (getline(infile, baris)) {
+			//dan tampilkan disini
+			cout << baris << '\n';
+		}
 	}
 }
 
